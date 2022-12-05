6ba3581aad9835e67a25948cfce286dc9bb45473
Amit-singh-05 <ami9sin05@gmail.com>
2022-12-06 01:39:26 +0530

    update

--- main.js
+++ main.js
@@ -36,3 +36,19 @@ window.addEventListener('scroll', () => {
 		alterStyles(isBackToTopRendered);
 	}
 });
+
+const hamburger = document.querySelector('.hamburger');
+const navItems = document.querySelector('.nav__items');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	navItems.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav__item').forEach((n) =>
+	n.addEventListener('click', () => {
+		hamburger.classList.remove('active');
+		navItems.classList.remove('active');
+	})
+);
+
